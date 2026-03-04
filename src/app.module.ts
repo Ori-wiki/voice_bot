203aa40487455f865cd6fa211aa51064e9f5b819
Denis <Ori21wiki@gmail.com>
2026-03-04 16:59:57 +0300

    add files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,14 @@
 import { Module } from '@nestjs/common';
-import { TelegrafModule } from 'nestjs-telegraf';
+import { ConfigModule } from '@nestjs/config';
+
 import LocalSession from 'telegraf-session-local';
 
 const sessions = new LocalSession();
 
 @Module({
   imports: [
-    TelegrafModule.forRoot({
-      middlewares: [sessions.middleware()],
-      token: '8295066134:AAH5M9DS13uJi34TpNYi3_7BACa9QM97Nps',
+    ConfigModule.forRoot({
+      isGlobal: true,
     }),
   ],
 })
